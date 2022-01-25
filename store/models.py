5a3f2c524f40629cdedd831cf02f962f86be89df
Kerriea-star <ojared511@gmail.com>
2022-01-25 19:08:25 +0300

    Rendering our views in our templates

--- store/models.py
+++ store/models.py
@@ -28,7 +28,7 @@ class Order(models.Model):
 	def __str__(self):
 		return str(self.id)
 
-class OderItem(models.Model):
+class OrderItem(models.Model):
 	product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
 	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
 	quantity = models.IntegerField(default=0, null=True, blank=True)
