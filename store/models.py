6403cb06a9d2db1e615ea8f18d1f85282eabf323
Kerriea-star <ojared511@gmail.com>
2022-01-28 11:10:51 +0300

    Configured images/media files in settings, linked them in our url and renderd them in our store templates files.

--- store/models.py
+++ store/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
 	name = models.CharField(max_length=200)
 	price = models.FloatField()
 	digital = models.BooleanField(default=False, null=True, blank=True)
-	#Image
+	image = models.ImageField(null=True, blank=True)
 
 	def __str__(self):
 		return self.name
